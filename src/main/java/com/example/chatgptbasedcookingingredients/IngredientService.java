96c5f3e929c59303802b890e4c1e0b778b2c9bb6
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-07-25 10:41:15 +0200

    implemented method which asks openAI whether an ingredient is vegan vegetarian oder regular.

--- src/main/java/com/example/chatgptbasedcookingingredients/IngredientService.java
+++ src/main/java/com/example/chatgptbasedcookingingredients/IngredientService.java
@@ -1,13 +1,39 @@
 package com.example.chatgptbasedcookingingredients;
 
+import com.example.chatgptbasedcookingingredients.model.OpenAiMessage;
+import com.example.chatgptbasedcookingingredients.model.OpenAiRequest;
+import com.example.chatgptbasedcookingingredients.model.OpenAiResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class IngredientService {
 
-    String categorizeIngredient(String ingredient) {
+    private final RestClient restClient;
+    public IngredientService(RestClient.Builder restClientBuilder,
+                             @Value("${OPENAI_API_KEY}") String apiKey) {
+        this.restClient = restClientBuilder
+                .baseUrl("https://api.openai.com/v1/chat/completions")
+                .defaultHeader("Authorization",
+                        "Bearer " + apiKey).build();
+    }
 
-        // TODO: This should return "vegan", "vegetarian" or "regular" depending on the ingredient.
-        return "vegan";
+    String categorizeIngredient(String ingredient) {
+        OpenAiMessage requestMessage = new OpenAiMessage("user",
+                "Gib als String zurück, ob die folgende Zutat: "
+                        + ingredient + " vegan, vegetarisch oder regular "
+                        + "ist.");
+        List<OpenAiMessage> messages = new ArrayList<>();
+        messages.add(requestMessage);
+        OpenAiRequest request = new OpenAiRequest("gpt-40-mini",
+                messages);
+        OpenAiResponse openAiResponse = restClient.post()
+                .body(request)
+                .retrieve().body(OpenAiResponse.class);
+        return openAiResponse.choices().get(0).message().content();
     }
 }
